e917fb7fb278bf8a1aac9248bc3b2ae8ebe2708c
Camila Irachet <cirachet@dlya.com.uy>
2017-05-05 14:36:27 -0300

    authenticate android

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,11 @@
 const constants = {
   auth: {
-    client_id: 'Ki7OYsnueyHlEwBHLcsoATBHpiuUq5gq',
-    client_secret: '4s9jyQ4QRV5NevmJq434oWepxgIFXgtb'
+    client_id: '3RRrFaxNTdMOUlGT85KHkKvIOiTEsUOp',
+    client_secret: 'VN51B519IYjMR9gK6hsFjRb70JxKF6Ls'
   },
   urls: {
-    authRedirect: 'http://localhost:8090/auth/dialog/authorize',
-    authToken: 'http://localhost:8090/auth/oauth/token'
+    authRedirect: 'http://10.12.10.114:8090/auth/dialog/authorize',
+    authToken: 'http://10.12.10.114:8090/auth/oauth/token'
   }
 }
 
