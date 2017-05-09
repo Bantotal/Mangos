a9f520a8977e680e58dc471da181b9b737eb2bcd
Developer <mvarela@dlya.com.uy>
2017-05-09 17:10:11 -0300

    promises to async/await

--- src/oAuth/index.js
+++ src/oAuth/index.js
@@ -4,44 +4,46 @@ import base64 from 'base-64'
 import constants from '../config/constants'
 
 const oAuth = {
-  bdevelopers: (opts) => {
+  bdevelopers: opts => {
     return new Promise((resolve, reject) => {
       const url = `${constants.urls.authRedirect}?client_id=${opts.client_id}&response_type=code&redirect_uri=mangos://authorize/`
 
       Linking.openURL(url)
         .catch(err => console.error('An error occurred', err))
 
-      Linking.addEventListener('url', (event) => {
-        const url = event.url
-        const code = url.substring(url.indexOf('=') + 1)
+      Linking.addEventListener('url', async event => {
+        try {
+          const url = event.url
+          const code = url.substring(url.indexOf('=') + 1)
 
-        const string = opts.client_id + ':' + opts.client_secret
-        const auth = 'Basic ' + base64.encode(string)
+          const string = opts.client_id + ':' + opts.client_secret
+          const auth = 'Basic ' + base64.encode(string)
 
-        fetch(constants.urls.authToken, { // eslint-disable-line
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': auth
-          },
-          body: JSON.stringify({
-            client: opts.client_id,
-            code,
-            redirect_uri: 'mangos://authorize/',
-            grant_type: 'authorization_code'
+          const result = await fetch(constants.urls.authToken, { // eslint-disable-line
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': auth
+            },
+            body: JSON.stringify({
+              client: opts.client_id,
+              code,
+              redirect_uri: 'mangos://authorize/',
+              grant_type: 'authorization_code'
+            })
           })
-        })
-          .then(response => {
-            if (response.status === 200) {
-              return response.json()
-            } else {
-              resolve(response)
-            }
-          })
-          .then(response => {
-            resolve(response)
-          })
-          .catch(err => reject(err))
+
+          if (result.status === 200) {
+            const resultJson = await result.json()
+            resolve(resultJson)
+          } else {
+            resolve(result)
+          }
+
+        } catch (err) {
+          console.log(`Linking.addEventListener error ${JSON.stringify(err)}`)
+          reject(err)
+        }
 
         Linking.removeEventListener('url', this._handleOpenURL)
       })
